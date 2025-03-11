51c1da28636761b8073e5cf86b6ff9f66d651039
Karlson Thien <prolexicgt@gmail.com>
2025-03-11 17:03:28 +0800

    Completed Project

--- Web_Scarping.py
+++ Web_Scarping.py
@@ -7,29 +7,35 @@ import requests
 # page id
 id = 1
 url = f"https://webscraper.io/test-sites/e-commerce/allinone/product/{id}"
-iny = 1
-ipy = 1
+iny = 0
+ipy = 0
+item_data = {}
 
 while True:
     wb = xlwt.Workbook()
     ws = wb.add_sheet('Sheet1')
-    print('---', id, '---')
 
     page = requests.get(url)
     soup = BeautifulSoup(page.text, "html.parser")
 
     item_name = str(soup.find("h4", attrs={"class":"title"}))[29:].split("<")[0]
-    print(item_name)
-    ws.write(iny,0,item_name)
-    item_price = float(str(soup.find("h4", attrs={"class":"price"}))[40:].split("<")[0])
-    print(item_price)
-    ws.write(ipy,1,item_price)
-    iny += 1
-    ipy += 1
+    try:
+        item_price = float(str(soup.find("h4", attrs={"class":"price"}))[40:].split("<")[0])
+    except ValueError as e:
+        item_price = None
 
-    wb.save("scrap_price.xls")
+    item_data[item_name] = item_price
+
+    print(f"Scraped: {id} item/s")
     if soup.find("h4", attrs={"class":"title"}):
         id += 1
         url = f"https://webscraper.io/test-sites/e-commerce/allinone/product/{id}"
     else:
+        for name, price in item_data.items():
+            ws.write(iny,0,name)
+            ws.write(ipy,1,price)
+            iny += 1
+            ipy += 1
+        wb.save("scrap_price.xls")
+        print(f"Completed")
         break
